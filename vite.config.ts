e9a168a2f4c628a80b3216b955c569f69ec69e6a
John Rueda <johnruedal96@gmail.com>
2026-04-27 20:18:00 -0500

    config shell

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import federation from '@originjs/vite-plugin-federation'
 
-const mfNutritionUrl = process.env.VITE_MF_NUTRITION_URL || 'http://localhost:3001/assets/remoteEntry.js'
+const mfNutritionUrl = process.env.VITE_MF_NUTRITION_URL || 'https://poli-vida-sana-mf-nutrition.vercel.app/assets/remoteEntry.js'
 const mfExerciseUrl = process.env.VITE_MF_EXERCISE_URL || 'https://poli-vida-sana-mf-exercise.vercel.app/assets/remoteEntry.js'
 const mfMetricsUrl = process.env.VITE_MF_METRICS_URL || 'https://poli-vida-sana-mf-metrics.vercel.app/assets/remoteEntry.js'
 
